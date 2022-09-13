8f6a7c67261a0924ded8985a493b6a5300394fc0
Hammu144 <hamzazmunir00@gmail.com>
2022-09-13 15:49:30 +0200

    wiki_graph description

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Data for djisktra
+#' Wiki_graph
+#' that data will use for djisktra
+#' @format A data frame with three enrtries:
+#' \describe{
+#' \item{v1}{contain data for first node}
+#' \item{v2}{contain data for second node}
+#' \item{w}{contain data for paths between nodes}
+#' }
+#'
+
+"wiki_graph"
